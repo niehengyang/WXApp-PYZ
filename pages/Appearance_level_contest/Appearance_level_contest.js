f7eb13e9b36aa0bc858c3e0d02d41cb2d5f4cfd2
NHY <790227542@qq.com>
2018-01-11 19:42:26 +0800

    拼颜值

--- pages/Appearance_level_contest/Appearance_level_contest.js
+++ pages/Appearance_level_contest/Appearance_level_contest.js
@@ -22,19 +22,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.request({
-      url: 'test.php', //仅为示例，并非真实的接口地址
-      data: {
-        x: '',
-        y: ''
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)
-      }
-    })
+    
 
   },
 
--- pages/ranking_page/ranking_page.js
+++ pages/ranking_page/ranking_page.js
@@ -6,12 +6,21 @@ Page({
    */
   data: {
     hiddenmodalput: true,
-    List: [{ data_id: 1, group_name: 'xxx', user_logo: '/images/mine/mine_logo.svg', appearance: 90, ranking: 1, user_name: 'vvv', pho_path: '/images/addpage/my_pho1.jpg' },
+    user_logo: '/images/mine/mine_logo.svg',
+    List: [{ data_id: 1, group_name: '动漫推荐动漫美图分享', user_logo: '/images/addpage/11.jpg', appearance: 90, ranking: 1, user_name: '为爱皮', pho_path: '/images/addpage/my_pho1.jpg' },
     { data_id: 2, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 80, ranking: 2, user_name: 'lll', pho_path: '/images/addpage/my_pho.jpg' },
     { data_id: 3, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 70, ranking: 3, user_name: 'lll', pho_path: '/images/addpage/my_pho2.jpg' },
     { data_id: 4, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 75, ranking: 4, user_name: 'lll', pho_path: '/images/addpage/my_pho3.jpg' },
-    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }, { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }
-    ],
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' },
+    { data_id: 5, group_name: 'xxx', user_logo: '/images/home/tu1.svg', appearance: 60, ranking: 5, user_name: 'lll', pho_path: '/images/addpage/my_pho4.jpg' }],
     index: 0,
     my_ifo: { ranking: 111, appearance: 50 },
     hiddenmodalput: true,
@@ -82,6 +91,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //请求组内数据
+    wx.request({
+      url: 'test.php', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        this.data.List=res.data;
+        console.log('获取的组内数据:'+res.data)
+        if (this.data.List.length == 0) {
+          this.setData({
+            no_ranking: !this.data.no_ranking,
+            user_logo: '/images/mine/mine_logo.svg'//默认头像
+          })
+        }
+      },
+      fail:function(res){
+        console.log('发生错误!'+res.data)
+      },
+       
+    })
     for (var i = 0; i < this.data.List.length; i++) {
       if (this.data.List[i].ranking == 1) {
         var logo = this.data.List[i].user_logo;
@@ -97,6 +131,8 @@ Page({
       }
     }
   },
+    //头部加载
+   
 
   /**
    * 生命周期函数--监听页面初次渲染完成
